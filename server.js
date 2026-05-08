f5f21d30af7b92f614dc4b152ce327875e962bdc
Brian Salazar <briansalazar5@gmail.com>
2026-05-07 20:13:10 -0700

    Convert routes for pages excluding projects from standard HTML to EJS

--- server.js
+++ server.js
@@ -5,8 +5,48 @@ const port = 3000;
 
 app.use(express.static("public"));
 
+// Index page
 app.get("/", (req, res) => {
     res.render("index.ejs");
+});
+
+// About
+app.get("/about", (req, res) => {
+    res.render("about.ejs");
+});
+
+// Resume
+app.get("/resume", (req, res) => {
+    res.render("resume.ejs");
+});
+
+// Certifications
+app.get("/certifications", (req, res) => {
+    res.render("certifications.ejs");
+});
+
+// Connect
+app.get("/connect", (req, res) => {
+    res.render("connect.ejs");
+});
+
+// Projects
+app.get("/projects", (req, res) => {
+    res.render("projects.ejs");
+});
+
+// Professional Project - NAME
+
+// Personal Project - NAME
+
+// Under connstruction
+app.get("/underconstruction", (req, res) => {
+    res.render("underconstruction.ejs");
+});
+
+// Page not found - 404
+app.use((req, res, next) => {
+  res.status(404).render("404.ejs"); // if using EJS
 });
 
 app.listen(port, () => {
