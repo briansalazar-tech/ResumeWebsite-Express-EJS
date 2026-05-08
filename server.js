f646f0f40df9b990b31f2d329da5c1d4540b81e2
Brian Salazar <briansalazar5@gmail.com>
2026-05-07 19:30:57 -0700

    Rearange content to conform to ejs requirements. Render index.ejs with header and footer partials

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ import express from "express";
 const app = express();
 const port = 3000;
 
-app.get("", (req, res) => {
-    res.send("Hello")
-})
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.render("index.ejs");
+});
 
 app.listen(port, () => {
-    console.log(`Listening on port ${port}`)
-})
+    console.log(`Listening on port ${port}`);
+});
