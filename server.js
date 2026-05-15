7d2771d80b04bfe8b3ef3f0b15c2fafad797a8d2
Brian Salazar <briansalazar5@gmail.com>
2026-05-14 18:14:09 -0700

    Fix error with project routes where /projects/ would remain when navigating away from a project causing 404 error

--- server.js
+++ server.js
@@ -36,61 +36,61 @@ app.get("/projects", (req, res) => {
 });
 
 // Professional Project - 1:1 DSMS
-app.get("/projects/1to1DSMS", (req, res) => {
+app.get("/1to1DSMS", (req, res) => {
     res.render("ProProj_1to1DSMS.ejs");
 });
 
 // Professional Project - 1:1 Traner
-app.get("/projects/1to1TranerMS", (req, res) => {
+app.get("/1to1TranerMS", (req, res) => {
     res.render("ProProj_1to1TranerMS.ejs");
 });
 
 // Professional Project - New Hug HS
-app.get("/projects/NewHugHS", (req, res) => {
+app.get("/NewHugHS", (req, res) => {
     res.render("ProProj_NewHugHS.ejs");
 });
 
 // Professional Project - New Hug HS
-app.get("/projects/TeacherRefresh", (req, res) => {
+app.get("/TeacherRefresh", (req, res) => {
     res.render("ProProj_TeacherRefresh.ejs");
 });
 // Personal Project - 100 Days of Code
-app.get("/projects/100DaysofCode", (req, res) => {
+app.get("/100DaysofCode", (req, res) => {
     res.render("PersProj_100DaysofCode.ejs");
 });
 
 // Personal Project - Cloud Resume Challenge
-app.get("/projects/CloudResumeChallenge", (req, res) => {
+app.get("/CloudResumeChallenge", (req, res) => {
     res.render("PersProj_CloudResumeChallenge.ejs");
 });
 
 // Personal Project - GitHub Projects
-app.get("/projects/GitHubProjects", (req, res) => {
+app.get("/GitHubProjects", (req, res) => {
     res.render("PersProj_GitHubProjects.ejs");
 });
 
 // Personal Project - PC Building
-app.get("/projects/PCBuilding", (req, res) => {
+app.get("/PCBuilding", (req, res) => {
     res.render("PersProj_PCBuilding.ejs");
 });
 
 // Personal Project - Portfolio Website 1.0
-app.get("/projects/PortfolioWebsiteV1", (req, res) => {
+app.get("/PortfolioWebsiteV1", (req, res) => {
     res.render("PersProj_PortfolioWebsiteV1.ejs");
 });
 
 // Personal Project - Portfolio Website 2.0
-app.get("/projects/PortfolioWebsiteV2", (req, res) => {
+app.get("/PortfolioWebsiteV2", (req, res) => {
     res.render("PersProj_PortfolioWebsiteV2.ejs");
 });
 
 // Personal Project - Windows Server Home Labs
-app.get("/projects/WindowsServerLabs", (req, res) => {
+app.get("/WindowsServerLabs", (req, res) => {
     res.render("PersProj_WindowsServerLabs.ejs");
 });
 
 // Personal Project - Windows Server Home Labs
-app.get("/projects/WGUCapstone", (req, res) => {
+app.get("/WGUCapstone", (req, res) => {
     res.render("PersProj_WGUCapstone.ejs");
 });
 
